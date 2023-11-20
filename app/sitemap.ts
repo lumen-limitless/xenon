5051c032191cb9423b08bd2c7d80c0182eb55d9d
lumen-limitless <lumenlimitless@gmail.com>
2023-11-20 18:56:56 -0500

    Add product URLs to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,27 @@
+import { prisma } from '@/lib/prisma'
 import { type MetadataRoute } from 'next'
 
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000'
 
-export default function Sitemap(): MetadataRoute.Sitemap {
+export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
+  const products = await prisma.product.findMany({
+    select: {
+      slug: true,
+      updatedAt: true,
+    },
+  })
+
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
     },
+
+    ...products.map((product) => ({
+      url: `${baseUrl}/products/${product.slug}`,
+      lastModified: product.updatedAt,
+    })),
   ]
 }
